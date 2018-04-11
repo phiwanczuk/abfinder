395b1cf9f0eab5326ca4eed8be3dc99ac59142e0
Piotrek <phiwanczuk@gmail.com>
2018-04-11 19:05:28 +0200

    ab form initialized

--- src/Components/AddBuddy/AddBuddy.js
+++ src/Components/AddBuddy/AddBuddy.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
 class AddBuddy extends React.Component{
+
+    state = {
+        buddyName: '',
+        buddyFreeTime: '',
+        buddyMaxDrink: '',
+        isChosen: false
+}
+
+handleInputChange = event => this.setState({
+    [event.target.name]: event.target.value
+})
+
+    handleSubmit = event => {
+        event.preventDefault();
+            this.props.addBuddyDetails(this.state)
+        alert('Buddy added')
+    }
+
+
+
     render(){
         return(
             <div>
-                <h1>Hello ab</h1>
+                <form onSubmit={this.handleSubmit}
+                >
+
+                </form>
             </div>
         )
     }
