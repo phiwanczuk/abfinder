5e37d4e179b1a52b48268f6e4d76fdf1076e2b5e
Piotrek <phiwanczuk@gmail.com>
2018-04-11 18:53:16 +0200

    add component joined

---
+++ src/Components/AddBuddy/AddBuddy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AddBuddy extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello ab</h1>
+            </div>
+        )
+    }
+}
+
+export default AddBuddy
