ef6958658d756248e630373e9ef254c94db3d499
Piotrek <phiwanczuk@gmail.com>
2018-04-11 19:28:03 +0200

    addbuddy component developed

--- src/Components/AddBuddy/AddBuddy.js
+++ src/Components/AddBuddy/AddBuddy.js
@@ -26,6 +26,30 @@ handleInputChange = event => this.setState({
             <div>
                 <form onSubmit={this.handleSubmit}
                 >
+                    <input
+                        name='buddyName'
+                        value={this.state.buddyName}
+                        placeholder='Buddy name...'
+                        type='text'
+                        onChange={this.handleInputChange}
+                    />
+                    <input
+                        pattern="[0-9]{1,2}"
+                        name='buddyFreeTime'
+                        type='text'
+                        placeholder='buddy free time, from ... to ...'
+                        value={this.state}
+                    />
+                    <input
+                    pattern="(?<=^| )\d+(\.\d+)?(?=$| )"
+                    name='carFuelConsumption'
+                    type='text'
+                    placeholder='car fuel consumption...'
+                    value={this.state.carFuelConsumption}
+                    onChange={this.handleInputChange}
+                />
+
+
 
                 </form>
             </div>
